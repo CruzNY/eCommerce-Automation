a09b618762a25e290af7c6864f9f1af1730a2b79
alex cruz <alexcruz182@gmail.com>
2019-10-13 20:50:50 -0400

    Init Commit

---
+++ automationECommerce/src/main/java/eCommerceAutomation/automationECommerce/App.java
@@ -0,0 +1,13 @@
+package eCommerceAutomation.automationECommerce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
