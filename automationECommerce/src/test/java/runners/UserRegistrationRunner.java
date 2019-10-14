008576ea95647deab4c38b736119acc39938b433
alex cruz <alexcruz182@gmail.com>
2019-10-13 22:41:19 -0400

    User Registration runner

--- automationECommerce/src/test/java/runners/UserRegistrationRunner.java
+++ automationECommerce/src/test/java/runners/UserRegistrationRunner.java
@@ -1,5 +1,15 @@
 package runners;
 
+import cucumber.api.CucumberOptions;
+
+@CucumberOptions(
+		features = {"/home/alex/git/phpTravales-Automation-Framework/AutomationFrameowrk/features"},
+		glue = {"steps"},
+		monochrome = true,
+		tags = {},
+		plugin = {"pretty","html:target/cucumber","json:target/cucumber.json","com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}
+		)
+
 public class UserRegistrationRunner {
 
 }
