55fd15030a62a15c0b654bfc4d465afc0be55bb2
alex cruz <alexcruz182@gmail.com>
2019-10-14 17:12:57 -0400

    Added Page Load Timing Method.

--- automationECommerce/src/main/java/steps/SimpleSteps.java
+++ automationECommerce/src/main/java/steps/SimpleSteps.java
@@ -1,37 +1,59 @@
 package steps;
 
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 import cucumber.api.PendingException;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.*;
+import junit.framework.Assert;
 
 public class SimpleSteps {
 	WebDriver driver;
+	
+	@Before()
 	public void setUp() {		
 		System.setProperty("webdriver.gecko.driver","/home/alex/git/phpTravales-Automation-Framework/AutomationFrameowrk/drivers/geckodriver");
 		this.driver = new FirefoxDriver();
 		this.driver.manage().window().maximize();
 		this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 	}
-	
+	 public void waitForPageLoaded() {
+	        ExpectedCondition<Boolean> expectation = new
+	                ExpectedCondition<Boolean>() {
+	                    public Boolean apply(WebDriver driver) {
+	                        return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete");
+	                    }
+	                };
+	        try {
+	            Thread.sleep(1000);
+	            WebDriverWait wait = new WebDriverWait(driver, 30);
+	            wait.until(expectation);
+	        } catch (Throwable error) {
+	            Assert.fail("Timeout waiting for Page Load Request to complete.");
+	        }
+	    }
 
 	@Given("^User is on the Homepage$")
 	public void user_is_on_the_Homepage() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		driver.get("http://automationpractice.com/index.php");
+		waitForPageLoaded();
 	}
 
 	@When("^User clicks on the \"([^\"]*)\" button$")
 	public void user_clicks_on_the_button(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+
 	}
 
 	@When("^User verifies the \"([^\"]*)\" page is displayed$")
 	public void user_verifies_the_page_is_displayed(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+//		driver.findElement(By.xpath("//h1"));
 	}
 
 	@When("^User enters \"([^\"]*)\" into textbox$")
--- automationECommerce/src/test/java/runners/UserRegistrationRunner.java
+++ automationECommerce/src/test/java/runners/UserRegistrationRunner.java
@@ -7,13 +7,13 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-		features = {"/home/alex/git/repository/automationECommerce/features/standardFeatures/UserRegistration.feature"},
+		features = {"/home/alex/git/repository/automationECommerce/features/standardFeatures"},
 		glue = {"steps"},
 		monochrome = true,
 		tags = {},
 		plugin = {"pretty","html:target/cucumber","json:target/cucumber.json","com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}
 		)
-
+ 
 public class UserRegistrationRunner {
 
 }
